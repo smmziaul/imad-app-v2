ef5a634ddceca615ea1274183d2247a9da41cb1f
SOOFI HUSSAIN S M M <smmziaul@gmail.com>
2017-04-09 23:01:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -234,3 +234,9 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+
+
+// testing spider animation
+app.get('/test', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
